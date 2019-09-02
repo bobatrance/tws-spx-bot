83e494d661e9d2689aee31a08962a7e1b87e1d42
bobatrance <darkfarmer1@gmail.com>
2019-09-02 01:54:57 -0700

    Adding logic for winner parsing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import praw
-import json
-import datetime
-import time
+import praw, json, datetime, time, urllib.request, math
+
+secondsBeforeCaptureResults = 60
+mapContestSubmissions = {}
 
 # loading credentials
 with open('../secret/credential.json', 'r') as f:
@@ -14,11 +14,43 @@ with open('../secret/credential.json', 'r') as f:
         username=jsonCredentials["username"],
         password=jsonCredentials["password"]
     )
+    alphavantage_api_key=jsonCredentials["apikey"]
 
+# function to handle contest submission parsing
+# check to see if it is a duplicate submission
+# check to see if message is a number for spx
+def checkDuplicateAndAddContestEntry(contestComment):
+    if contestComment.author not in mapContestSubmissions:
+        try:
+            floatContestCommentBody = float(contestComment.body)
+            mapContestSubmissions[contestComment.author] = floatContestCommentBody
+        except ValueError:
+            print("Skipping " + contestComment.author + "'s comment of " + contestComment.body)
+
+def getFloatTodaySpxClose():
+    with urllib.request.urlopen("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SPX&outputsize=compact&apikey="+ alphavantage_api_key) as url:
+        data = json.loads(url.read().decode())
+    return float(data.get("Time Series (Daily)").get(datetime.date.today().strftime("%Y-%m-%d")).get("4. close"))
+
+def findClosestNumber():
+    listClosestAuthors = []
+    floatClosestNumber = 9999999.0
+    for submissionAuthor in mapContestSubmissions:
+        delta = math.fabs(mapContestSubmissions[submissionAuthor]-getFloatTodaySpxClose())
+        if delta < floatClosestNumber:
+            floatClosestNumber = delta
+            listClosestAuthors.clear()
+            listClosestAuthors.append(submissionAuthor)
+        elif delta == floatClosestNumber:
+            listClosestAuthors.append(submissionAuthor)
+    return listClosestAuthors
+
+# main
 # setting up daily post parse
 subreddit = reddit.subreddit("thewallstreet")
 sTodayDate = datetime.date.today().strftime("%B %d")
 sDailyDiscussionTarget = "Daily Discussion - (" + sTodayDate + ")"
+print("Targeting " + sDailyDiscussionTarget)
 
 for submission in subreddit.hot(limit=15):
     if submission.title == sDailyDiscussionTarget:
@@ -28,13 +60,19 @@ for submission in subreddit.hot(limit=15):
                 if not subreddit.user_is_banned:
                     print("we're ok to post")
                     commentStartContest = submission.reply("\U0001F4C8 Contest now open! \U0001F4C9")
-                    time.sleep(60) #seconds
-                    #do logic capture all comments
+                    #time.sleep(secondsBeforeCaptureResults) # delay timer for post emulation
+
+                    # do logic capture all comments
                     commentStartContest.refresh()
-                    commentReplies = commentStartContest.replies
-                    #toplevel replies only
-                    for replies in commentReplies:
-                        print(replies.body)
+                    commentContestReplies = commentStartContest.replies
+                    for comment in commentContestReplies:
+                        # take top level replies and hash to make sure there are no repeat
+                        checkDuplicateAndAddContestEntry(comment)
+
+                    # do logic close contest and parse winner
                     commentCloseContest = "~~" + commentStartContest.body + "~~" + "\n\nEdit: Contest now closed!"
                     commentStartContest.edit(commentCloseContest)
+                    listWinners = findClosestNumber()
+                    for winner in listWinners:
+                        print(winner)
                     break
